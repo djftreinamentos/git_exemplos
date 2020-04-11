5b6dd7800712fa39f59bb0bd7bfac27a045df9c4
Felipe da Rosa Oliveira <felipe@djftreinamentos.com.br>
2020-04-11 20:28:01 -0300

    criado o arquivo de configuraçao do servidor

---
+++ servidor.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000,()=>console.log('Servidor rodando na porta 3000 http://localhost:300'));
